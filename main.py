cae23db50522d711578b251fdf721431e4f48908
Seiro Ogasawara <seiro.ogasawara@abeja.asia>
2022-11-24 15:53:22 +0900

    query parameters (#2)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from fastapi import FastAPI
+from typing import Union
 
 
 class ModelName(str, Enum):
@@ -10,15 +11,29 @@ class ModelName(str, Enum):
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/items/")
+async def read_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 
 @app.get("/users/me")
@@ -45,3 +60,19 @@ async def get_model(model_name: ModelName):
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+    return item
+
+
+@app.get("/items_with_needy_param/{item_id}")
+async def read_user_item_with_needy(item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
